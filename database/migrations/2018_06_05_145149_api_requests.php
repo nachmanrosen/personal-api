0eb9c3cf6d1ec3133b52285e69837b57ee1bd8d2
Nachman Rosen <nachmanrosen@gmail.com>
2018-06-06 12:48:57 -0400

    Update 2018_06_05_145149_api_requests.php

--- database/migrations/2018_06_05_145149_api_requests.php
+++ database/migrations/2018_06_05_145149_api_requests.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ApiRequests extends Migration
+class ApiRequest extends Migration
 {
     /**
      * Run the migrations.
