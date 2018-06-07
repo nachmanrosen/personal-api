c77b64f7c3782e04ac76ddfc1516d46960e1ed0b
vagrant <vagrant@homestead>
2018-06-07 18:04:17 +0000

    new

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Personal API</title>
+        <style>
+        body{
+           background-color:lightblue;
+           text-align: center; 
+        }
+        </style>
+    </head>
+    <body>
+        
+
+        <div class="container">
+            @yield('content')
+           
+        </div>
+    </body>
+</html>
---
+++ resources/views/home2.blade.php
@@ -0,0 +1,10 @@
+@extends('home')
+
+@section('content')
+    <h1> Welcome to Nachman Rosen's Personal API</h1>
+    <br>
+    <p>This RESTful API provides data about Nachman Rosen's technological skills, projects, and job experience.</p>
+    <p>Documentation:</p>
+    <p>https://app.swaggerhub.com/apis/Nachman/nachman-rosen_open_api/1.0</p>
+    <a href="https://app.swaggerhub.com/apis/Nachman/nachman-rosen_open_api/1.0"></a>
+@endsection
