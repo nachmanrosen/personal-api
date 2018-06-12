028429b503faac18b35061c2a1f76788facc782b
vagrant <vagrant@homestead>
2018-06-12 16:55:33 +0000

    1.1

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1> Welcome to Nachman Rosen's Personal API</h1>
     <br>
-    <p>This RESTful API provides data about Nachman Rosen's technological skills, projects, and job experience.</p>
+    <p>This RESTful API provides data about Nachman Rosen's technological skills, projects, and job experience. The user can also POST, PUT, PATCH, and DELETE posts</p>
     
-    <a href="https://app.swaggerhub.com/apis/Nachman/nachman-rosen_open_api/1.0">DOCUMENTATION</a>
+    <a href="https://app.swaggerhub.com/apis/Nachman/nachman-rosen_open_api/1.1">DOCUMENTATION</a>
 @endsection
